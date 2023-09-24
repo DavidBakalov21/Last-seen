ea4cd7a60cdc12b2d46073f5447d82e4fffa2f5f
DavidBakalov21 <davidbakalov33@gmail.com>
2023-09-24 16:19:26 +0300

    automatically changing offset

--- main.py
+++ main.py
@@ -6,5 +6,22 @@ def fetch_json(url):
     json_data = response.json()
     return json_data
 
-data = fetch_json("https://sef.podkolzin.consulting/api/users/lastSeen?offset=0")
-print(data)
+
+
+UserList=[]
+offset=0
+starter=0
+while True:
+    url = "https://sef.podkolzin.consulting/api/users/lastSeen?offset="+str(offset)
+    data = fetch_json(url)
+    if (starter==0 and  len(data["data"])>0) or  len(data["data"])>0 :
+        UserList.extend(data["data"])
+    else:
+        print("Failed to fetch data.")
+        break
+    offset+=len(data["data"])
+    starter+=1
+    print(data)
+
+
+
