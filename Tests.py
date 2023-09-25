a875c049ba67bb7c3a7b22d03682896cfab217aa
DavidBakalov21 <davidbakalov33@gmail.com>
2023-09-25 11:22:46 +0300

    now there are lots of file

--- Tests.py
+++ Tests.py
@@ -1,70 +1,71 @@
 import unittest
-import main
 from datetime import timedelta, timezone, datetime
-
-
+import FormatData
+import ConvertToReadable
+import OffsetLoop
+import fetch_json
 class TestLastSeen(unittest.TestCase):
 
     def test_True(self):
-        result = main.ConvertToReadable(True, "user","1")
+        result = ConvertToReadable.ConvertToReadable(True, "user","1")
         self.assertEqual(result, "User " + "user" + " is online")
 
-        result2 = main.ConvertToReadable(True, "user", "2")
+        result2 = ConvertToReadable.ConvertToReadable(True, "user", "2")
         self.assertEqual(result2, "Користувач " + "user" + " онлайн")
 
 
     def test_CoupleOfMinutes(self):
-        result = main.ConvertToReadable(timedelta(hours=0, minutes=16, seconds=59, microseconds=384718), "user","1")
+        result = ConvertToReadable.ConvertToReadable(timedelta(hours=0, minutes=16, seconds=59, microseconds=384718), "user","1")
         self.assertEqual(result, "User " + "user"+ " was last seen at " + "0:16:59.384718" + " (couple of minutes ago)")
 
-        result2 = main.ConvertToReadable(timedelta(hours=0, minutes=16, seconds=59, microseconds=384718), "user","2")
+        result2 = ConvertToReadable.ConvertToReadable(timedelta(hours=0, minutes=16, seconds=59, microseconds=384718), "user","2")
         self.assertEqual(result2, "Користувач " + "user"+ " в останнє був присутній " + "0:16:59.384718" + " (декілька хвилин тому)")
     def test_LessThenMinutes(self):
-        result = main.ConvertToReadable(timedelta(hours=0, minutes=0, seconds=30, microseconds=384718), "user","1")
+        result = ConvertToReadable.ConvertToReadable(timedelta(hours=0, minutes=0, seconds=30, microseconds=384718), "user","1")
         self.assertEqual(result,"User " + "user" + " was last seen at " + "0:00:30.384718" + " (less than a minute ago)")
 
-        result2 = main.ConvertToReadable(timedelta(hours=0, minutes=0, seconds=30, microseconds=384718), "user", "2")
+        result2 = ConvertToReadable.ConvertToReadable(timedelta(hours=0, minutes=0, seconds=30, microseconds=384718), "user", "2")
         self.assertEqual(result2,"Користувач " + "user" + " в останнє був присутній " + "0:00:30.384718" + " (менше хвилини тому)")
 
 
     def test_Yesterday(self):
-        result = main.ConvertToReadable(timedelta(days=1, hours=1, minutes=0, seconds=30, microseconds=384718), "user","1")
+        result = ConvertToReadable.ConvertToReadable(timedelta(days=1, hours=1, minutes=0, seconds=30, microseconds=384718), "user","1")
         self.assertEqual(result,"User " + "user" + " was last seen at " + "1 day, 1:00:30.384718" + " (yesterday)")
 
-        result2 = main.ConvertToReadable(timedelta(days=1, hours=1, minutes=0, seconds=30, microseconds=384718), "user", "2")
+        result2 = ConvertToReadable.ConvertToReadable(timedelta(days=1, hours=1, minutes=0, seconds=30, microseconds=384718), "user", "2")
         self.assertEqual(result2,"Користувач " + "user" + " в останнє був присутній " + "1 day, 1:00:30.384718" + " (вчора)")
 
 
     def test_Hour(self):
-        result = main.ConvertToReadable(timedelta(days=0, hours=1, minutes=0, seconds=30, microseconds=384718), "user","1")
+        result = ConvertToReadable.ConvertToReadable(timedelta(days=0, hours=1, minutes=0, seconds=30, microseconds=384718), "user","1")
         self.assertEqual(result,"User " + "user" + " was last seen at " + "1:00:30.384718" + " (hour ago)")
 
-        result2 = main.ConvertToReadable(timedelta(days=0, hours=1, minutes=0, seconds=30, microseconds=384718), "user","2")
+        result2 = ConvertToReadable.ConvertToReadable(timedelta(days=0, hours=1, minutes=0, seconds=30, microseconds=384718), "user","2")
         self.assertEqual(result2,"Користувач " + "user" + " в останнє був присутній " + "1:00:30.384718" + " (годину тому)")
 
     def test_today(self):
-        result = main.ConvertToReadable(timedelta(days=0, hours=2, minutes=0, seconds=59, microseconds=384718), "user","1")
+        result =ConvertToReadable.ConvertToReadable(timedelta(days=0, hours=2, minutes=0, seconds=59, microseconds=384718), "user","1")
         self.assertEqual(result,"User " + "user" + " was last seen at " + "2:00:59.384718" + " (today)")
 
-        result2 = main.ConvertToReadable(timedelta(days=0, hours=2, minutes=0, seconds=59, microseconds=384718), "user","2")
+        result2 = ConvertToReadable.ConvertToReadable(timedelta(days=0, hours=2, minutes=0, seconds=59, microseconds=384718), "user","2")
         self.assertEqual(result2,"Користувач " + "user" + " в останнє був присутній " + "2:00:59.384718" + " (сьогодні)")
 
     def test_This_Week(self):
-        result = main.ConvertToReadable(timedelta(days=2, hours=2, minutes=0, seconds=59, microseconds=384718), "user","1")
+        result =ConvertToReadable.ConvertToReadable(timedelta(days=2, hours=2, minutes=0, seconds=59, microseconds=384718), "user","1")
         self.assertEqual(result,"User " + "user" + " was last seen at " + "2 days, 2:00:59.384718" + " (this week)")
 
-        result2 = main.ConvertToReadable(timedelta(days=2, hours=2, minutes=0, seconds=59, microseconds=384718), "user","2")
+        result2 = ConvertToReadable.ConvertToReadable(timedelta(days=2, hours=2, minutes=0, seconds=59, microseconds=384718), "user","2")
         self.assertEqual(result2,"Користувач " + "user" + " в останнє був присутній " + "2 days, 2:00:59.384718" + " (цього тижня)")
 
     def test_LongAgo(self):
-        result = main.ConvertToReadable(timedelta(days=8, hours=2, minutes=0, seconds=59, microseconds=384718), "user","1")
+        result = ConvertToReadable.ConvertToReadable(timedelta(days=8, hours=2, minutes=0, seconds=59, microseconds=384718), "user","1")
         self.assertEqual(result,"User " + "user" + " was last seen at " + "8 days, 2:00:59.384718" + " (Long ago)")
 
-        result2 = main.ConvertToReadable(timedelta(days=8, hours=2, minutes=0, seconds=59, microseconds=384718), "user","2")
+        result2 = ConvertToReadable.ConvertToReadable(timedelta(days=8, hours=2, minutes=0, seconds=59, microseconds=384718), "user","2")
         self.assertEqual(result2,"Користувач " + "user" + " в останнє був присутній " + "8 days, 2:00:59.384718" + " (давно)")
 
     def test_FormatDataOffline(self):
-        result = main.FormatData(
+        result = FormatData.FormatData(
             {'userId': '2fba2529-c166-8574-2da2-eac544d82634', 'nickname': 'Doug93', 'firstName': 'Doug',
              'lastName': 'Rogahn', 'registrationDate': '2023-06-04T03:53:45.4490942+00:00',
              'lastSeenDate': '2023-09-24T15:22:42.9695297+00:00', 'isOnline': False})
@@ -72,21 +73,21 @@ class TestLastSeen(unittest.TestCase):
             "2023-09-24T15:22:42.9695297+00:00"))
 
     def test_FormatDataOnline(self):
-        result = main.FormatData(
+        result =  FormatData.FormatData(
             {'userId': '2fba2529-c166-8574-2da2-eac544d82634', 'nickname': 'Doug93', 'firstName': 'Doug',
              'lastName': 'Rogahn', 'registrationDate': '2023-06-04T03:53:45.4490942+00:00',
              'lastSeenDate': '2023-09-24T15:22:42.9695297+00:00', 'isOnline': True})
         self.assertEqual(result, True)
 
     def test_GetJason(self):
-        result = main.fetch_json("https://sef.podkolzin.consulting/api/users/lastSeen?offset=0")
+        result = fetch_json.fetch_json("https://sef.podkolzin.consulting/api/users/lastSeen?offset=0")
         self.assertEqual(result["data"][0]["nickname"], "Doug93")
     def test_GetJason2(self):
-        result = main.fetch_json("https://sef.podkolzin.consulting/api/users/lastSeen?offset=20")
+        result = fetch_json.fetch_json("https://sef.podkolzin.consulting/api/users/lastSeen?offset=20")
         self.assertEqual(result["data"][0]["nickname"], "Karl94")
 
     def test_OffsetLoop(self):
-        result = main.OffsetLoop()
+        result = OffsetLoop.OffsetLoop()
         self.assertEqual(result[0]["nickname"], "Doug93")
 
 
--- main.py
+++ main.py
@@ -1,109 +1,11 @@
-import requests
-from datetime import datetime, timezone
-
-
-def fetch_json(url):
-    response = requests.get(url)
-    response.raise_for_status()
-    json_data = response.json()
-    return json_data
-
-
-
-def FormatData(data):
-    if data['isOnline']==False:
-        LSeen = datetime.fromisoformat(data['lastSeenDate'])
-        currentTime = datetime.now().astimezone(timezone.utc)
-        Differenc=currentTime-LSeen
-
-        return Differenc
-    else:
-        return data['isOnline']
-
-
-def ConvertToReadable(info, user, language):
-    if info!=True:
-        if info.days<1 and info.seconds<30:
-            if language=="1":
-                return "User " + user + " was last seen at " + str(info)+" (just now)"
-            else:
-                return "Користувач " + user + " в останнє був присутній " + str(info) + " (тільки що)"
-
-        elif info.days<1 and info.seconds < 60 and info.seconds>1:
-            if language == "1":
-                return "User " + user + " was last seen at " + str(info) + " (less than a minute ago)" #Ok
-            else:
-                return "Користувач " + user + " в останнє був присутній " + str(info) + " (менше хвилини тому)"
-
-        elif info.days<1 and info.seconds < 3540 and info.seconds>60:
-            if language == "1":
-                return "User " + user + " was last seen at " + str(info) + " (couple of minutes ago)" #Ok
-            else:
-                return "Користувач " + user + " в останнє був присутній " + str(info) + " (декілька хвилин тому)"  # Ok
-
-        elif info.days<1 and info.seconds < 7140 and info.seconds>3600:
-            if language == "1":
-                return "User " + user + " was last seen at " + str(info) + " (hour ago)" #Ok
-            else:
-                return "Користувач " + user + " в останнє був присутній " + str(info) + " (годину тому)" # Ok
-
-        elif info.seconds>7200 and info.days<1:
-            if language == "1":
-                return "User " + user + " was last seen at " + str(info) + " (today)" #Ok
-            else:
-                return "Користувач " + user + " в останнє був присутній " + str(info) + " (сьогодні)" # Ok
-
-        elif info.days==1:
-            if language == "1":
-                return "User " + user + " was last seen at " + str(info) + " (yesterday)" #Ok
-            else:
-                return "Користувач " + user + " в останнє був присутній " + str(info) + " (вчора)" # Ok
-
-        elif info.days>1 and info.days<7:
-            if language == "1":
-                return "User " + user + " was last seen at " + str(info) + " (this week)" #OK
-            else:
-                return "Користувач " + user + " в останнє був присутній " + str(info) + " (цього тижня)" # Ok
-
-        else:
-            if language == "1":
-                return "User " + user + " was last seen at " + str(info) + " (Long ago)" #OK
-            else:
-                return "Користувач " + user + " в останнє був присутній " + str(info) + " (давно)" # Ok
-    else:
-        if language == "1":
-            return "User " + user + " is online" #Ok
-        else:
-            return "Користувач " + user + " онлайн"  # Ok
+import FormatData
+import ConvertToReadable
+import OffsetLoop
 language=input("1-English, 2-Ukrainian ")
-
-
-
-def OffsetLoop():
-    UserList = []
-    offset = 0
-    starter = 0
-    while True:
-        url = "https://sef.podkolzin.consulting/api/users/lastSeen?offset=" + str(offset)
-        data = fetch_json(url)
-        if (starter == 0 and len(data["data"]) > 0) or len(data["data"]) > 0:
-            UserList.extend(data["data"])
-        else:
-           # print("Failed to fetch data.")
-            break
-        offset += len(data["data"])
-        starter += 1
-    return UserList
-    
-UserList=OffsetLoop()
+UserList=OffsetLoop.OffsetLoop()
 FormatedList={}
 for i in UserList:
-    #print(FormatData(i))
-    FormatedList[i['nickname']]=FormatData(i)
 
-#print(FormatedList)
-
-#print(len(FormatedList))
+    FormatedList[i['nickname']]=FormatData.FormatData(i)
 for i in FormatedList:
-
-    print(ConvertToReadable(FormatedList[i], i, language))
+    print(ConvertToReadable.ConvertToReadable(FormatedList[i], i, language))
