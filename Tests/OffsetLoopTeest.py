04255685322999c809e43b0ec3445ca59905759b
DavidBakalov21 <davidbakalov33@gmail.com>
2023-09-26 14:15:10 +0300

    Offset tests

--- Tests/OffsetLoopTeest.py
+++ Tests/OffsetLoopTeest.py
@@ -11,3 +11,50 @@ class TestOffsetLoop(unittest.TestCase):
             {"data": [{"nickname": "Doug93"}, {"nickname": "Karl94"}]}, {"data": []}]
         result = OffsetLoop()
         self.assertEqual(result, [{"nickname": "Doug93"}, {"nickname": "Karl94"}])
+
+    @patch('Functions.OffsetLoop.fetch_json')
+    def test_OffsetLoop_DefferentNumberOfItems_Doug93AndKarl94(self, MockGet):
+        MockGet.side_effect = [
+            {"data": [{"nickname": "Doug93"}, {"nickname": "Karl94"}]},
+            {"data": [{"nickname": "Doug993"}]},
+            {"data": [{"nickname": "Doug193"}, {"nickname": "Doug1033"}, {"nickname": "Karl994"}]},
+            {"data": []}]
+        result = OffsetLoop()
+        self.assertEqual(result, [{"nickname": "Doug93"}, {"nickname": "Karl94"},{"nickname": "Doug993"},{"nickname": "Doug193"},{"nickname": "Doug1033"}, {"nickname": "Karl994"}])
+
+    @patch('Functions.OffsetLoop.fetch_json')
+    def test_OffsetLoop_Empty_Doug93AndKarl94(self, MockGet):
+        MockGet.side_effect = [
+
+            {"data": []}]
+        result = OffsetLoop()
+        self.assertEqual(result, [])
+
+    @patch('Functions.OffsetLoop.fetch_json')
+    def test_OffsetLoop_TwoFullPages_Doug93AndKarl94(self, MockGet):
+        MockGet.side_effect = [
+
+            {"data":  [{"nickname": "Doug93"}, {"nickname": "Karl94"}, {"nickname": "Karl94"}, {"nickname": "Karl94"},
+                      {"nickname": "Karl94"}, {"nickname": "Karl94"}, {"nickname": "Karl94"}, {"nickname": "Karl94"},
+                      {"nickname": "Karl94"}, {"nickname": "Karl94"}, {"nickname": "Karl94"}, {"nickname": "Karl94"},
+                      {"nickname": "Karl94"}, {"nickname": "Karl94"}, {"nickname": "Karl94"}, {"nickname": "Karl94"},
+                      {"nickname": "Karl94"}, {"nickname": "Karl94"}, {"nickname": "Karl94"}, {"nickname": "Karl94"}]},
+
+            {"data": [{"nickname": "Doug93"}, {"nickname": "Karl94"}, {"nickname": "Karl94"}, {"nickname": "Karl94"},
+                      {"nickname": "Karl94"}, {"nickname": "Karl94"}, {"nickname": "Karl94"}, {"nickname": "Karl94"},
+                      {"nickname": "Karl94"}, {"nickname": "Karl94"}, {"nickname": "Karl94"}, {"nickname": "Karl94"},
+                      {"nickname": "Karl94"}, {"nickname": "Karl94"}, {"nickname": "Karl94"}, {"nickname": "Karl94"},
+                      {"nickname": "Karl94"}, {"nickname": "Karl94"}, {"nickname": "Karl94"}, {"nickname": "Karl94"}]},
+
+            {"data": []}]
+
+        result = OffsetLoop()
+        self.assertEqual(result, [{"nickname": "Doug93"}, {"nickname": "Karl94"}, {"nickname": "Karl94"}, {"nickname": "Karl94"},
+                      {"nickname": "Karl94"}, {"nickname": "Karl94"}, {"nickname": "Karl94"}, {"nickname": "Karl94"},
+                      {"nickname": "Karl94"}, {"nickname": "Karl94"}, {"nickname": "Karl94"}, {"nickname": "Karl94"},
+                      {"nickname": "Karl94"}, {"nickname": "Karl94"}, {"nickname": "Karl94"}, {"nickname": "Karl94"},
+                      {"nickname": "Karl94"}, {"nickname": "Karl94"}, {"nickname": "Karl94"}, {"nickname": "Karl94"},{"nickname": "Doug93"}, {"nickname": "Karl94"}, {"nickname": "Karl94"}, {"nickname": "Karl94"},
+                      {"nickname": "Karl94"}, {"nickname": "Karl94"}, {"nickname": "Karl94"}, {"nickname": "Karl94"},
+                      {"nickname": "Karl94"}, {"nickname": "Karl94"}, {"nickname": "Karl94"}, {"nickname": "Karl94"},
+                      {"nickname": "Karl94"}, {"nickname": "Karl94"}, {"nickname": "Karl94"}, {"nickname": "Karl94"},
+                      {"nickname": "Karl94"}, {"nickname": "Karl94"}, {"nickname": "Karl94"}, {"nickname": "Karl94"}])
