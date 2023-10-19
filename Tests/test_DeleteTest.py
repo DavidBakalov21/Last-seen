a9595430dd1c93d289e10298d73455a9a70ec392
DavidBakalov21 <davidbakalov33@gmail.com>
2023-10-19 17:16:19 +0300

    try5m

--- Tests/test_DeleteTest.py
+++ Tests/test_DeleteTest.py
@@ -1,11 +1,14 @@
+import os
 
 import Functions.DeleteUser
 import unittest
 class TestDelete(unittest.TestCase):
-
+    DIR = os.path.dirname(os.path.abspath(__file__))
     def test_Delete_NormId_WasDone(self):
-        result = Functions.DeleteUser.DeleteData('DelTest.csv', '2fba2529-c166-8574-2da2-eac544d82634')
+        csv = os.path.join(TestDelete.DIR, 'DelTest.csv')
+        result = Functions.DeleteUser.DeleteData(csv, '2fba2529-c166-8574-2da2-eac544d82634')
         self.assertEqual(result,'it was done')
     def test_Delete_BadId_NotDone(self):
-        result = Functions.DeleteUser.DeleteData('DelTest.csv', '2fba2529-c166-8sashjkd574-2da2-easdfgc544d82634')
+        csv = os.path.join(TestDelete.DIR, 'DelTest.csv')
+        result = Functions.DeleteUser.DeleteData(csv, '2fba2529-c166-8sashjkd574-2da2-easdfgc544d82634')
         self.assertEqual(result, "user wasn't present or id was incorect")
